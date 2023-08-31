7c9dc72df1f69a241916996c22857f96491e8f6a
goenning <me@goenning.net>
2023-08-31 15:36:22 +0100

    enable preventAssignment

--- packages/web/rollup.config.mjs
+++ packages/web/rollup.config.mjs
@@ -1,35 +1,36 @@
-import replace from "@rollup/plugin-replace";
-import terser from "@rollup/plugin-terser";
-import typescript from "@rollup/plugin-typescript";
-import pkg from "./package.json" assert { type: "json" };
+import replace from '@rollup/plugin-replace';
+import terser from '@rollup/plugin-terser';
+import typescript from '@rollup/plugin-typescript';
+import pkg from './package.json' assert { type: 'json' };
 
 const plugins = [
   terser(),
   replace({
-    "env.PKG_VERSION": pkg.version,
+    'env.PKG_VERSION': pkg.version,
+    preventAssignment: true,
   }),
   typescript({
-    tsconfig: "./tsconfig.json",
-    moduleResolution: "node",
+    tsconfig: './tsconfig.json',
+    moduleResolution: 'node',
   }),
 ];
 
 const cjs = {
-  input: "./src/index.ts",
+  input: './src/index.ts',
   output: {
-    dir: "./dist",
-    entryFileNames: "[name].js",
-    format: "cjs",
+    dir: './dist',
+    entryFileNames: '[name].js',
+    format: 'cjs',
   },
   plugins,
 };
 
 const es = {
-  input: "./src/index.ts",
+  input: './src/index.ts',
   output: {
-    dir: "./dist",
-    entryFileNames: "[name].mjs",
-    format: "es",
+    dir: './dist',
+    entryFileNames: '[name].mjs',
+    format: 'es',
   },
   plugins,
 };
