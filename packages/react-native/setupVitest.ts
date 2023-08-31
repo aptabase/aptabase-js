9dcef380f4587e10add02e4f712194a8e183556f
goenning <me@goenning.net>
2023-08-31 15:34:11 +0100

    remove non-web packages

--- packages/react-native/setupVitest.ts
+++
@@ -1,8 +0,0 @@
-import createFetchMock from "vitest-fetch-mock";
-import { vi } from "vitest";
-
-vi.stubGlobal("__DEV__", true);
-
-const fetchMocker = createFetchMock(vi);
-
-fetchMocker.enableMocks();
--- packages/tauri/webview-src/index.ts
+++
@@ -1,9 +0,0 @@
-import { invoke } from '@tauri-apps/api'
-
-type Props = {
-  [key: string]: string | number
-}
-
-export async function trackEvent(name: string, props?: Props): Promise<void> {
-  await invoke<string>('plugin:aptabase|track_event', { name, props })
-}
